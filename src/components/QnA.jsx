fcc7e3dd38c8ee4f9d09d9438c9553a517dcb924
Niloy Chowdhury <125306577+NiloyChowdhuryVR@users.noreply.github.com>
2023-07-08 01:36:39 +0530

    Update QnA.jsx

--- src/components/QnA.jsx
+++ src/components/QnA.jsx
@@ -29,7 +29,7 @@ const QnA = () => {
     firstQsHeader.style.display = 'none';
     console.log(formvalue.question);
     try{
-      const response = await fetch('http://localhost:8080/gita',{
+      const response = await fetch('https://dharmaback.onrender.com/gita',{
       method: 'POST',
       body:JSON.stringify(formvalue),
       headers:{
